e243de8442427cc3945b747244e5a6338fd76470
Joel Loh Quan Yu <xolotlwastaken@gmail.com>
2025-03-07 15:52:52 +0800

    frontend

---
+++ src/components/LoadingIndicator.jsx
@@ -0,0 +1,10 @@
+import "../styles/LoadingIdicator.css";
+
+const LoadingIndicator = () => {
+    return <div className="loading-container">
+        <div className="loader"></div>
+    </div>
+
+}
+
+export default LoadingIndicator;
